c3635aaa15564c45ea2f6e61da6337bd0e82920f
tamtonaquib <aquibjavedt007@gmail.com>
2021-03-16 12:43:29 +0530

    mapped esc to jk in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,3 +21,8 @@ end
 
 source $HOME/.config/fish/aliases.fish
 source $HOME/.config/fish/exports.fish
+
+# Vi mode mapped Esc to jk
+function fish_user_key_bindings
+    bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+end
