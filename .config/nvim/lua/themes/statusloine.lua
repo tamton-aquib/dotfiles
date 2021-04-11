79f6011505697de334467bd1ee42a81c06ed3428
tamtonaquib <aquibjavedt007@gmail.com>
2021-04-11 21:33:20 +0530

    updated statusline with 2 intermediate colors

--- .config/nvim/lua/themes/statusloine.lua
+++ .config/nvim/lua/themes/statusloine.lua
@@ -1,23 +1,23 @@
 local cmd = vim.api.nvim_command
-
 local M = {}
 
 local leftSeparator = ""
 local rightSeparator = ""
 
-local green  = "#2bbb4f"	--> "#6ed57e"
-local violet = "#986fec"
-local blue   = "#4799eb"	--> "#03353e"
-local yellow = "#fff94c"	--> "#ffd55b"
-local black  = "#000000"
-local red    = "#e27d60"
+local green     = "#2bbb4f"	--> "#6ed57e"
+local violet    = "#986fec"
+local blue      = "#4799eb"	--> "#03353e"
+local yellow    = "#fff94c"	--> "#ffd55b"
+local black     = "#000000"
+local red       = "#e27d60"
+local lightGrey = "#303030"
 
 local getColor = {
-     ['n']    =  violet,
+     ['n']    =  green,
      ['v']    =  blue,
      ['V']    =  blue,
-     ['i']    =  green,
-     ['ic']   =  green,
+     ['i']    =  violet,
+     ['ic']   =  violet,
      ['c']    =  red,
      ['t']    =  yellow,
      ['r']    =  yellow,
@@ -48,7 +48,7 @@ local modes = {
      ['V']   = ' ',
      ['i']   = ' ',
      ['ic']  = '',
-     ['c']   = ' ',
+     ['c']   = ' ',
      ['r']   = 'Prompt',
      ['t']   = 'T',
      ['R']   = ' ',
@@ -57,7 +57,7 @@ local modes = {
      -- ["<C-v>"]  = ' ',
 }
 
-function setDefault (t, d)
+function ifNotFound (t, d)
   local mt = {__index = function () return d end}
   setmetatable(t, mt)
 end
@@ -66,23 +66,25 @@ function M.get_statusline()
 	local mode = vim.api.nvim_get_mode()['mode']
 	local extension = vim.api.nvim_call_function('expand', {'%:e'})
 
-	setDefault(modes, ' ')
-	setDefault(getFileIcon, ' ')
-	setDefault(getColor, red)
+	ifNotFound(modes, ' ')
+	ifNotFound(getFileIcon, ' ')
+	ifNotFound(getColor, red)
 
     local modeIcon	= modes[mode]
     local modeColor = getColor[mode]
 	local fileIcon	= getFileIcon[extension]
 
-	local s = '%#Noice#  '..modeIcon..' %#Arrow#'..leftSeparator..' %M'
+	local s = '%#Noice#  '..modeIcon..' %#Arrow#'..leftSeparator
+	s = s..'%#MidArrow#'..leftSeparator..' %M'
 
     s = s..'%='
 
-    s = s..'%#Arrow#'..rightSeparator..'%#Noice# '
+    s = s..rightSeparator..'%#Arrow#'..rightSeparator..'%#Noice# '
     s = s..fileIcon..'  [%l/%L] :%c 並%p%%  '
 
     cmd('hi Noice guibg='..modeColor..' guifg=#000000')
-    cmd('hi Arrow guifg='..modeColor)
+    cmd('hi Arrow guifg='..modeColor..' guibg='..lightGrey)
+	cmd('hi MidArrow guifg='..lightGrey)
 	return s
 end
 
