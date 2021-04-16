d016ddfb7c77a87430fe82e333bc9c858510d85b
tamtonaquib <aquibjavedt007@gmail.com>
2021-04-16 16:12:09 +0530

    removed kommentary and added custom function

--- .config/nvim/lua/general/functions.lua
+++ .config/nvim/lua/general/functions.lua
@@ -1,71 +1,44 @@
 local cmd = vim.api.nvim_command
 local mapp = vim.api.nvim_set_keymap
 
--- let s:comment_map = { 
---     \   "c": '\/\/',
---     \   "java": '\/\/',
---     \   "javascript": '\/\/',
---     \   "php": '\/\/',
---     \   "python": '#',
---     \   "sh": '#',
---     \   "vim": '"',
---     \   "rust": '\/\/',
---     \   "lua": '--',
--- 	\	"conf": '#'
---     \ }
+local comment_map = { 
+		c			= '//',
+		java		= '//',
+		javascript	= '//',
+		python		= '//',
+		py			= '#',
+		sh			= '#',
+		vim			= '"',
+		rust		= '//',
+		lua			= '%-%-',
+		conf		= '#'
+}
 
--- function! ToggleComment()
---     if has_key(s:comment_map, &filetype)
---         let comment_leader = s:comment_map[&filetype]
---         if getline('.') =~ "^\\s*" . comment_leader . " " 
---             Uncomment the line
---             execute "silent s/^\\(\\s*\\)" . comment_leader . " /\\1/"
---         else 
---             if getline('.') =~ "^\\s*" . comment_leader
---                 Uncomment the line
---                 execute "silent s/^\\(\\s*\\)" . comment_leader . "/\\1/"
---             else
---                 Comment the line
---                 execute "silent s/^\\(\\s*\\)/\\1" . comment_leader . " /"
---             end
---         end
---     else
---         echo "No comment leader found for filetype"
---     end
--- endfunction
+function toggle_comment()
+	local extension = vim.api.nvim_call_function('expand', {'%:e'})
+	local comment_match = comment_map[extension]
+	local comment_leader = comment_match:gsub("%%", "")
+    local _, starting, _, _ = unpack(vim.fn.getpos("'<"))
+    local _, ending, _, _	= unpack(vim.fn.getpos("'>"))
 
--- function toggle_comment()
---     local extension = vim.api.nvim_call_function('expand', {'%:e'})
---     -- local comment_leader = comment_map[extension]
--- 
---     local comment_leader = "-"
---     local line = vim.api.nvim_get_current_line()
--- 
---     local _, starting, _, _ = unpack(vim.fn.getpos("'<"))
---     local _, ending, _, _ = unpack(vim.fn.getpos("'>"))
--- 
---     local pattern = ""
---     local hoe = string.find(line, "^"..comment_leader)
---     print(hoe)
--- 
---     if (string.find(line, "^"..comment_leader) ~= nil)
---         then
---         if (starting ~= 0) then
---             pattern = ':'..starting..','..ending..'s/^'..comment_leader..'//g'
---         else
---             pattern = ':.s/^'..comment_leader..'//g'
---         end
---     else
---         if (starting ~= 0) then
---             pattern = ':'..starting..','..ending..'s/^/'..comment_leader..'/g'
---         else
---             pattern = ':.s/^/'..comment_leader..'/g'
---         end
---     end
--- end
+    local line = vim.api.nvim_get_current_line()
 
--- mapp('v', '<C-_>', ':lua toggle_comment()<CR>', {noremap=true})
--- mapp('n', '<C-_>', ':lua toggle_comment()<CR>', {noremap=true})
+	if starting == 0 and ending == 0 then
+		starting = "."
+		ending = "."
+	end
+
+	if line:match("^"..comment_match) then
+		local uncomment = starting..','..ending..'s/^'..comment_leader..' //g'
+		cmd(uncomment)
+	else
+		local comment = starting..','..ending..'s/^/'..comment_leader..' /g'
+		cmd(comment)
+	end
+end
+
+mapp('v', '<C-_>', ':lua toggle_comment()<CR>', {noremap=true, silent=true})
+mapp('n', '<C-_>', ':lua toggle_comment()<CR>', {noremap=true})
 
 
 local is_tranparent = 1
