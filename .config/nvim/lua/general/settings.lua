6cc02fbbcd7acee900dba8287cea07a7ba5a4f6a
tamton-aquib <aquibjavedt007@gmail.com>
2021-06-05 15:38:25 +0530

    updating nvim settings to opt

--- .config/nvim/lua/general/settings.lua
+++ .config/nvim/lua/general/settings.lua
@@ -1,60 +1,63 @@
 local cmd = vim.api.nvim_command
+local current = vim.api.nvim_get_current_buf
 
 cmd('set iskeyword+=-')
 cmd('syntax enable')
 cmd("filetype plugin indent on")
 cmd('set inccommand=split')
-cmd('set nomore')
 
--- vim.o.formatoptions = "cro"
+vim.o.formatoptions = "cro"
+vim.go.t_Co = "256"
+vim.opt.more = false
 
-vim.o.mouse = "a"
-vim.o.encoding = "UTF-8"
-vim.o.fileencoding = "UTF-8"
-vim.o.termguicolors = true
-vim.o.background = "dark"
-vim.o.hidden = true
-vim.o.scrolloff = 8
-vim.o.timeoutlen = 300
-vim.o.updatetime = 500
-vim.o.hlsearch = false
-vim.o.clipboard = "unnamedplus"
-vim.o.smarttab = true
-vim.o.backup = false
-vim.o.writebackup = false
-vim.o.conceallevel = 0
-vim.o.cmdheight = 1
-vim.o.splitright = true
-vim.o.splitbelow = true
-vim.o.t_Co = "256"
-vim.o.pumheight = 10
-vim.o.incsearch = true
-vim.o.showmode = false
-vim.o.showtabline = 2
-vim.o.laststatus = 2
-vim.o.foldenable = true
-vim.o.foldmethod = "expr"
-vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-vim.o.foldlevelstart = 25
+vim.opt.mouse = "a"
+vim.opt.encoding = "UTF-8"
+vim.opt.fileencoding = "UTF-8"
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.hidden = true
+vim.opt.scrolloff = 8
+vim.opt.timeoutlen = 300
+vim.opt.updatetime = 500
+vim.opt.hlsearch = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.smarttab = true
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.conceallevel = 0
+vim.opt.cmdheight = 1
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.pumheight = 10
+vim.opt.incsearch = true
+vim.opt.showmode = false
+vim.opt.showtabline = 2
+vim.opt.laststatus = 2
+vim.opt.foldenable = true
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevelstart = 25
 
 function on_file_enter()
-    vim.bo.expandtab = false
-    vim.bo.copyindent = true
-    vim.bo.shiftwidth = 4
-    vim.bo.tabstop = 4
-    vim.bo.softtabstop = 4
-    vim.bo.autoindent = true
-    vim.bo.smartindent = true
+	vim.opt.expandtab = false
+	vim.opt.copyindent = true
+	vim.opt.shiftwidth = 4
+	vim.opt.tabstop = 4
+	vim.opt.softtabstop = 4
+	vim.opt.autoindent = true
+	vim.opt.smartindent = true
 
-    vim.cmd("norm '\"")
+	if vim.fn.getpos('"') then 
+		vim.cmd("norm '\"")
+	end
+
 end
 vim.cmd [[ autocmd FileType * :lua on_file_enter() ]]
 
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.wo.wrap = false
-vim.wo.cursorline = true
-vim.wo.list = true
-vim.wo.listchars = 'tab:┊ '
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.cursorline = true
+vim.opt.list = true
+vim.opt.listchars = 'tab:┊ '
 
--- cmd('autocmd BufNewFile,BufRead * setlocal formatoptions-=cro')
