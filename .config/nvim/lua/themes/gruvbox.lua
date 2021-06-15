216d938c635dfb30cf31f1c894c5b7550d351597
tamton-aquib <aquibjavedt007@gmail.com>
2021-06-15 15:18:41 +0530

    moving nvim configs to sep repo

--- .config/nvim/lua/themes/gruvbox.lua
+++
@@ -1,7 +0,0 @@
-local cmd = vim.api.nvim_command
-
--- vim.g.gruvbox_transparent = true
--- vim.g.gruvbox_hide_inactive_statusline = true
-vim.g.gruvbox_hide_inactive_statusline = true
-cmd [[colo gruvbox-flat]]
-
